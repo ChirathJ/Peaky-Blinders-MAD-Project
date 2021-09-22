c248ebe4b471d162514e4541aa004f50e3bdeaaf
IT20228576 <IT20228576@gmail.com>
2021-09-22 23:04:22 +0530

    initial

--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -1,19 +1,11 @@
 package com.example.mad;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
